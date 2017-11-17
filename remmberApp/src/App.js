5c72f498321cc47f5f1cbd03363e4e84a27a09da
Perry Ahern <pahern@gmail.com>
2017-11-16 19:50:42 -0500

    Files relocated for server setup; app now in remmberApp folder.

--- remmberApp/src/App.js
+++ remmberApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 import MemoriesList from './components/Memories/Memories-List.js';
-// import memoryData from './components/Memories/memory-data.js';
-// import userData from './components/Users/user-data.js';
 
 class App extends Component {
   render() {
@@ -13,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Remmber</h1>
           <h3 className="App-subtitle">What &amp; When?</h3>
         </header>
-        <div>    {/* mount the memories and avatar links to the virtual DOM */}
+        <div>
           <MemoriesList />
         </div>
       </div>
